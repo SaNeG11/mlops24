a5b11ba179f317e9809f9e70147aebea75b94cfa
saneg11 <stil222@mail.ru>
2024-01-22 20:19:32 +0000

    update scripts

--- scripts/data-processing/text_to_numeric.py
+++ scripts/data-processing/text_to_numeric.py
@@ -1,13 +1,38 @@
-import pandas as pd
+import sys
 import os
+import io
 
-os.chdir('~/proj/mlops24/scripts/data-processing')
-os.makedirs('../../data/stage3', exist_ok=True)
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write("\tpython3 change_text_to_numeric.py data-file\n")
+    sys.exit(1)
 
-df = pd.read_csv('../../data/stage2/train.csv')
+f_input = sys.argv[1]
+f_output = os.path.join("datasets", "stage3", "train.csv")
+os.makedirs(os.path.join("datasets", "stage3"), exist_ok=True)
 
-df.loc[df.Sex == 'male', 'Sex'] = 1
-df.loc[df.Sex == 'female', 'Sex'] = 0
-df.Sex = df.Sex.astype(int)
+def process_data(fd_in, fd_out):
+    arr_survived = []
+    arr_pclass = []
+    arr_sex = []
+    arr_age = []
 
-df.to_csv('../../data/stage3/train.csv', index=False)
+    for line in fd_in:
+        line = line.rstrip('\n').split(',')
+        arr_survived.append(line[0])
+        arr_pclass.append(line[1])
+        arr_sex.append(line[2])
+        arr_age.append(line[3])
+
+    for i in range(len(arr_sex)):
+        if arr_sex[i] == 'male':
+            arr_sex[i] = 0
+        else:
+            arr_sex[i] = 1
+
+    for p_survived, p_pclass, p_sex, p_age in zip(arr_survived, arr_pclass, arr_sex, arr_age):
+        fd_out.write("{},{},{},{}\n".format(p_survived, p_pclass, p_sex, p_age))
+
+with io.open(f_input, encoding="utf8") as fd_in:
+    with io.open(f_output, "w", encoding="utf8") as fd_out:
+        process_data(fd_in, fd_out)
